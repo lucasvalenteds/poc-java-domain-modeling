d1463d267e00596d27a559f18f871ab661702762
Lucas <lucasvalenteds@gmail.com>
2022-04-03 18:00:17 -0300

    Implementing RuntimeExceptionMapper
    This implementation is temporary. It returns any RuntimeException
    derivations as Bad Request (HTTP status code 400).

---
+++ src/main/java/com/example/infrastructure/validation/RuntimeExceptionMapper.java
@@ -0,0 +1,24 @@
+package com.example.infrastructure.validation;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+@Provider
+public final class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RuntimeExceptionMapper.class);
+
+    @Override
+    public Response toResponse(RuntimeException exception) {
+        LOGGER.info(exception.getMessage(), exception);
+
+        return Response.status(Response.Status.BAD_REQUEST)
+            .entity(Map.of("message", exception.getMessage()))
+            .build();
+    }
+}
