a5162bddd88ed5b6fde80060766f8e537964115c
Lucas <lucasvalenteds@gmail.com>
2022-04-03 18:05:30 -0300

    Minor improvements in Main class
    * Removing code comments
    * Replacing System.out with logger
    * Defining application properties to provision in-memory database

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,45 +1,40 @@
 package com.example;
 
+import com.example.infrastructure.configuration.DatabaseFeature;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
 
-/**
- * Main class.
- *
- */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static final String BASE_URI = "http://localhost:8080/";
 
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
     public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in com.example package
-        final ResourceConfig rc = new ResourceConfig().packages("com.example");
+        final ResourceConfig resourceConfig = new ResourceConfig()
+            .packages(Main.class.getPackageName())
+            .property(DatabaseFeature.DATABASE_URL_PROPERTY, "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1")
+            .property(DatabaseFeature.DATABASE_USER_PROPERTY, "sa")
+            .property(DatabaseFeature.DATABASE_PASSWORD_PROPERTY, "password");
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
     }
 
-    /**
-     * Main method.
-     * @param args
-     * @throws IOException
-     */
     public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with endpoints available at "
-                + "%s%nHit Ctrl-C to stop it...", BASE_URI));
+        final var server = Main.startServer();
+
+        LOGGER.info("Application started with endpoints available at {}", Main.BASE_URI);
+        LOGGER.info("Hit Ctrl-C to stop it...");
+
+        //noinspection ResultOfMethodCallIgnored
         System.in.read();
-        server.stop();
+
+        server.shutdownNow();
     }
 }
-
