8a9967d75b36cff7b6807e7bf6bc26d6e04019b7
Lucas <lucasvalenteds@gmail.com>
2022-04-04 19:20:17 -0300

    Setting Course Title max length to 80

--- src/main/java/com/example/domain/course/Title.java
+++ src/main/java/com/example/domain/course/Title.java
@@ -2,6 +2,7 @@ package com.example.domain.course;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
-public record Title(@NotNull @NotEmpty String value) {
+public record Title(@NotNull @NotEmpty @Length(min = 0, max = 80) String value) {
 }
